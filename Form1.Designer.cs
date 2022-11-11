47c45e9a563256ab6492865e00c02fa5ee637c32
Jonathan Ruiz Santos <jruizs1800@alumno.ipn.mx>
2022-11-11 14:17:52 -0600

    V1

---
+++ Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace _05_Perceptron_Iirs_Plant
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.CBOpciones = new System.Windows.Forms.ComboBox();
+            this.btPerceptron = new System.Windows.Forms.Button();
+            this.lBdatos = new System.Windows.Forms.ListBox();
+            this.lBresultados = new System.Windows.Forms.ListBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(20, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Elegir Clases";
+            // 
+            // CBOpciones
+            // 
+            this.CBOpciones.FormattingEnabled = true;
+            this.CBOpciones.Items.AddRange(new object[] {
+            "Setosa-Versicolor",
+            "Setosa-Virginica",
+            "Versicolor-Virginica"});
+            this.CBOpciones.Location = new System.Drawing.Point(12, 122);
+            this.CBOpciones.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.CBOpciones.Name = "CBOpciones";
+            this.CBOpciones.Size = new System.Drawing.Size(133, 23);
+            this.CBOpciones.TabIndex = 1;
+            // 
+            // btPerceptron
+            // 
+            this.btPerceptron.BackColor = System.Drawing.Color.Cyan;
+            this.btPerceptron.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.btPerceptron.Location = new System.Drawing.Point(24, 189);
+            this.btPerceptron.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btPerceptron.Name = "btPerceptron";
+            this.btPerceptron.Size = new System.Drawing.Size(95, 56);
+            this.btPerceptron.TabIndex = 2;
+            this.btPerceptron.Text = "Perceptrón";
+            this.btPerceptron.UseVisualStyleBackColor = false;
+            this.btPerceptron.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lBdatos
+            // 
+            this.lBdatos.FormattingEnabled = true;
+            this.lBdatos.ItemHeight = 15;
+            this.lBdatos.Location = new System.Drawing.Point(171, 22);
+            this.lBdatos.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.lBdatos.Name = "lBdatos";
+            this.lBdatos.Size = new System.Drawing.Size(244, 394);
+            this.lBdatos.TabIndex = 3;
+            // 
+            // lBresultados
+            // 
+            this.lBresultados.FormattingEnabled = true;
+            this.lBresultados.ItemHeight = 15;
+            this.lBresultados.Location = new System.Drawing.Point(428, 22);
+            this.lBresultados.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.lBresultados.Name = "lBresultados";
+            this.lBresultados.Size = new System.Drawing.Size(244, 394);
+            this.lBresultados.TabIndex = 4;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(695, 22);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(500, 393);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(1221, 427);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.lBresultados);
+            this.Controls.Add(this.lBdatos);
+            this.Controls.Add(this.btPerceptron);
+            this.Controls.Add(this.CBOpciones);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Perceptron Iris-Plant";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private ComboBox CBOpciones;
+        private Button btPerceptron;
+        private ListBox lBdatos;
+        private ListBox lBresultados;
+        private DataGridView dataGridView1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,336 @@
+﻿namespace _05_Perceptron_Iirs_Plant
+{
+    public partial class Form1 : Form
+    {
+
+        int entradas;
+        double filas;
+        double salidaY = 0;
+
+        int contadorCorrectas;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            mostrarDatos();
+
+        }
+
+        // Funcion mostrar Archivo
+        public void mostrarDatos()     
+        {
+            //Lectura de Archivo iris.txt
+            StreamReader leeLinea = new StreamReader("iris.txt");
+            string linea;
+            int contador = -1;
+            try
+            {
+                linea = leeLinea.ReadLine();
+                while (linea != null)
+                {
+                    lBdatos.Items.Add(linea + "\n");
+                    linea = leeLinea.ReadLine();
+                    contador++;
+                }
+                lBdatos.Items.Add("Numero de datos: " + contador);
+            }
+            catch
+            {
+                MessageBox.Show("¡Error, no se encuentra el archivo!    ಥ‿ಥ ");
+            }
+            leeLinea.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            entradas = 4;
+            filas = 100;
+            limpia();
+            contadorCorrectas = 0;
+
+            CreaTabla(entradas, filas);
+            LlenarTabla(entradas, filas);
+            Y_esperada(entradas);
+            Y_calculada(entradas, filas);
+
+        }
+
+        public void limpia()
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+            lBresultados.Items.Clear();
+        }
+
+        public void CreaTabla(int entradas, double filas)
+        {
+            for (int i = 0; i < entradas; i++)
+            {
+                dataGridView1.Columns.Add(new DataGridViewTextBoxColumn() { HeaderText = "X" + (i + 1).ToString() });
+            }
+
+            for (int j = 1; j < filas; j++)
+            {
+                j = dataGridView1.Rows.Add(new DataGridViewRow());
+            }
+
+            dataGridView1.Columns.Add(new DataGridViewTextBoxColumn() { HeaderText = "Yesperada" });
+            dataGridView1.Columns.Add(new DataGridViewTextBoxColumn() { HeaderText = "Ycalculada" });
+        }
+
+        public void LlenarTabla(int entradas, double filas)
+        {
+            StreamReader leeLinea = new StreamReader("iris.txt");
+            string linea;
+
+            linea = leeLinea.ReadLine();
+            string[] datos = linea.Split(',');
+            string clase = datos[datos.Length - 1];
+
+                if (CBOpciones.Text == "Setosa-Versicolor")
+                {
+
+                    for (int i = 0; i < 50; i++)
+                    {
+
+                        for (int j = 0; j < entradas; j++)
+                        {
+
+                            dataGridView1.Rows[i].Cells[j].Value = datos[j];
+
+                        }
+                        linea = leeLinea.ReadLine();
+                        datos = linea.Split(',');
+                    }
+
+                    for (int i = 50; i < 100; i++)
+                    {
+
+                        for (int j = 0; j < entradas; j++)
+                        {
+
+                            dataGridView1.Rows[i].Cells[j].Value = datos[j];
+
+                        }
+                        linea = leeLinea.ReadLine();
+                        datos = linea.Split(',');
+                    }
+                }
+
+                else if(CBOpciones.Text == "Setosa-Virginica")
+                {
+
+                    for (int i = 0; i < 49; i++)
+                    {
+
+                        for (int j = 0; j < entradas; j++)
+                        {
+
+                            dataGridView1.Rows[i].Cells[j].Value = datos[j];
+
+                        }
+                        linea = leeLinea.ReadLine();
+                        datos = linea.Split(',');
+                    }
+
+                    for(int k = 50; k < 100; k++)
+                    {
+                        linea = leeLinea.ReadLine();
+                    }
+
+                    for (int i = 49; i < 100; i++)
+                    {
+
+                        for (int j = 0; j < entradas; j++)
+                        {
+
+                            dataGridView1.Rows[i].Cells[j].Value = datos[j];
+
+                        }
+                        linea = leeLinea.ReadLine();
+                        datos = linea.Split(',');
+                    }
+
+                }
+
+                else if(CBOpciones.Text == "Versicolor-Virginica")
+                {
+                    for (int k = 0; k < 49; k++)
+                    {
+                        linea = leeLinea.ReadLine();
+                    }
+
+                linea = leeLinea.ReadLine();
+                datos = linea.Split(',');
+
+                for (int i = 0; i < 50; i++)
+                    {
+
+                        for (int j = 0; j < entradas; j++)
+                        {
+
+                            dataGridView1.Rows[i].Cells[j].Value = datos[j];
+
+                        }
+                        linea = leeLinea.ReadLine();
+                        datos = linea.Split(',');
+                    }
+
+                    for (int i = 50; i < 100; i++)
+                    {
+
+                        for (int j = 0; j < entradas; j++)
+                        {
+
+                            dataGridView1.Rows[i].Cells[j].Value = datos[j];
+
+                        }
+                        linea = leeLinea.ReadLine();
+                        datos = linea.Split(',');
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Opción no disponible.");
+                }
+            leeLinea.Close();
+        }
+
+        public void Y_esperada(int entradas)
+        {
+            for(int i = 0; i < 50; i++)
+            {
+                dataGridView1.Rows[i].Cells[entradas].Value = "-1";
+            }
+
+            for (int i = 50; i < 100; i++)
+            {
+                dataGridView1.Rows[i].Cells[entradas].Value = "1";
+            }
+        }
+
+        public void Y_calculada(int entradas, double filas)
+        {
+            Random umbral = new Random();
+            Random pesos = new Random();
+            Random pesos1 = new Random();
+            Random umbral1 = new Random();
+            double valorTabla = 0;
+
+            //.........
+            //Pesos aleatoreos
+            double[] pesosAleatoreos = new double[75];
+            double valorUmbral;
+            double[] valoresUmbral = new double[75];
+            int posicionPesos, posicionUmbral;
+
+            for (int i = 0; i < 75; i++)
+            {
+                pesosAleatoreos[i] = pesos.NextDouble();
+                valoresUmbral[i] = umbral.NextDouble();
+            }
+
+            pesosAleatoreos[60] = 0;
+            pesosAleatoreos[27] = 1;
+
+            valoresUmbral[47] = 0;
+            valoresUmbral[15] = 1;
+
+
+            //........
+            double[] pesos_array = new double[entradas];
+
+            int contador = 0;
+
+            int n = 0;
+
+            //Calcula los pesos aleatoreos 
+            for (int i = 0; i < entradas; i++)
+            {
+                posicionPesos = pesos1.Next(0, 75);
+                pesos_array[i] = pesosAleatoreos[posicionPesos];
+            }
+
+            posicionUmbral = umbral1.Next(0, 75);
+            valorUmbral = valoresUmbral[posicionUmbral];
+
+            //Proceso de aprendizaje completo
+            do
+            {
+
+                if (contadorCorrectas == filas)
+                {
+                    MessageBox.Show("Solución encontrada!!!");
+                    break;
+                }
+
+                contadorCorrectas = 0;
+
+                //Muestra valores de los pesos y umbral en listBox
+                for (int i = 0; i < entradas; i++)
+                {
+                    lBresultados.Items.Add("Peso[" + i + "]: " + pesos_array[i]);
+                }
+                lBresultados.Items.Add("Umbral: " + valorUmbral);
+                lBresultados.Items.Add("Entrada: " + contador);
+                lBresultados.Items.Add(" ");
+
+                //Pone los valores en Y_calculada
+                for (int i = 0; i < filas; i++)
+                {
+                    for (int j = 0; j < entradas; j++)
+                    {
+                        // Obtencion de los valores del datagrid tabla
+                        valorTabla = double.Parse((string)dataGridView1.Rows[i].Cells[j].Value);
+                        // Obtencion del valor de la suma por renglon
+                        salidaY = salidaY + (pesos_array[j] * valorTabla);
+                    }
+                    salidaY = salidaY + valorUmbral;
+
+                    if (salidaY > 0)
+                    {
+                        dataGridView1.Rows[i].Cells[entradas + 1].Value = "1";
+                        salidaY = 0;
+                    }
+                    else
+                    {
+                        dataGridView1.Rows[i].Cells[entradas + 1].Value = "-1";
+                        salidaY = 0;
+                    }
+
+
+
+                    //Compara Y_esperada y Y_calculada
+                    if (double.Parse((string)dataGridView1.Rows[i].Cells[entradas].Value) != double.Parse((string)dataGridView1.Rows[i].Cells[entradas + 1].Value))
+                    {
+                        for (int j = 0; j < entradas; j++)
+                        {
+                            double[] cambio_peso = new double[entradas];
+                            cambio_peso[j] = double.Parse((string)dataGridView1.Rows[i].Cells[entradas].Value) * double.Parse((string)dataGridView1.Rows[i].Cells[j].Value);
+                            pesos_array[j] = pesos_array[j] + cambio_peso[j];
+                        }
+                        valorUmbral = valorUmbral + double.Parse((string)dataGridView1.Rows[i].Cells[entradas].Value);
+                    }
+                    else if (double.Parse((string)dataGridView1.Rows[i].Cells[entradas].Value) == double.Parse((string)dataGridView1.Rows[i].Cells[entradas + 1].Value))
+                    {
+                        contadorCorrectas++;
+
+                    }
+
+                }
+
+                contador++;
+                if (contador == 99)
+                {
+                    MessageBox.Show("No tiene solución");
+                }
+            } while (contador < 100);
+            n = 0;
+        }
+
+    }
+}
